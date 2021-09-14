8033035529b3464a232c131f671737ac1ce889af
luit <luit93@gmail.com>
2021-09-14 14:06:16 +1000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "react-bootstrap";
+function App() {
+  return (
+    <div className="App">
+      Hello world!
+      <Button variant="primary">
+        <i class="fas fa-users"></i> User
+      </Button>
+    </div>
+  );
+}
+
+export default App;
